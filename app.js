5c8b307571bb7ea49e6280370dfb52f2b60ee59b
Trystan Eveno <trystan.eveno@lephare.com>
2018-01-25 17:30:30 +0100

    hide pwd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 5000;
-const DB_LINK = 'mongodb://beealive:beetches@ds115198.mlab.com:15198/beealive';
+const DB_LINK = 'mongodb://<dbuser>:<dbpassword>@ds115198.mlab.com:15198/beealive';
 
 const express = require("express");
 const app = express();
